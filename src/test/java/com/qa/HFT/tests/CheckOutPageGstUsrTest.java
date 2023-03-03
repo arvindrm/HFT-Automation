7bab7d160154bf97098a5055e5d15708703f256e
daradeepak123 <daradeepak123@outlook.com>
2023-03-03 22:41:02 +0530

    Modified checkoutpage guest user test.

--- src/test/java/com/qa/HFT/tests/CheckOutPageGstUsrTest.java
+++ src/test/java/com/qa/HFT/tests/CheckOutPageGstUsrTest.java
@@ -1,5 +1,65 @@
 package com.qa.HFT.tests;
 
-public class CheckOutPageGstUsrTest {
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.qa.HFT.base.BaseTest;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+
+public class CheckOutPageGstUsrTest extends BaseTest{
+	
+	@BeforeClass
+	public void accSetup() {
+		//myCartPage = loginPage.doLoginCart(prop.getProperty("username"), prop.getProperty("password"));
+		
+	}
+	
+	@DataProvider public Object[][] getProductAddToCartTestData(){
+		return new Object[][] {
+			{"64113","Aravind","Mamilapally","599 calle de las ovejas", "3132128989"}
+		};
+	}
+	
+	@Test(dataProvider = "getProductAddToCartTestData")
+	@Description("Verifying Product is Add to cart")
+	@Severity(SeverityLevel.CRITICAL)
+	
+	public void productAddToCartTest(String searchKey,String firstName, String lastName, String Address, String telephone) throws InterruptedException {
+		//myCartPage.hello();
+		//myCartPage.addItemToCartFlow();
+		myCartPage.performSearch(searchKey);
+		Thread.sleep(10000);
+		myCartPage.addToCart();
+		myCartPage.viewCartCheckOutBtn();
+		checkOutPage =myCartPage.secureClick();
+		Thread.sleep(3000);
+		checkOutPage.doCreateAddressClick().click();
+		//checkOutPage=myCartPage.SKUperformSearch(searchKey);
+		//myCartPage.addToCart();
+		//myCartPage.viewCartCheckOutBtn();
+		//checkOutPage.
+		checkOutPage.enterShippingAddress(firstName, lastName, Address, telephone);
+		checkOutPage.doDeliveryMethodClick().click();
+		Thread.sleep(10000);
+		checkOutPage.doPaymentClick().click();
+		Thread.sleep(3000);
+		checkOutPage.switchtoframe();
+		Thread.sleep(3000);
+		
+		checkOutPage.paymentconfirmBtn();
+		Thread.sleep(10000);
+		checkOutPage.doRewviewClick();
+		Thread.sleep(30000);
+		
+		
+	}
+	
+	
+	
+	
 
 }
