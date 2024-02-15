c9b0aa1ce8a2b7ad05c18adc785076cc8d53543e
Aravind Mamilapally <amamilapally@harborfreight.com>
2024-02-15 15:46:08 -0800

    added addArguments(--no-sandbox);

--- src/main/java/com/qa/HFT/factory/OptionsManager.java
+++ src/main/java/com/qa/HFT/factory/OptionsManager.java
@@ -19,9 +19,11 @@ public class OptionsManager {
 		co = new ChromeOptions();
 		//co.setBrowserVersion("116");
 		co.addArguments("--remote-allow-origins=*");
+		co.addArguments("--no-sandbox");
 		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
 			System.out.println(".....Running the test in Headless mode.......");
 			co.setHeadless(true);
+			System.out.println(co.);
 		}
 		if (Boolean.parseBoolean(prop.getProperty("incognito"))) {
 			System.out.println(".....Running the test in Incognito mode.......");
