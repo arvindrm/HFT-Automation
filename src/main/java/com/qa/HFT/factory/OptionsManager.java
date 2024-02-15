b5a89d40723ea59c16f286ccbb88ef027e178b10
Aravind Mamilapally <amamilapally@harborfreight.com>
2024-02-15 15:54:12 -0800

    added addArguments(--no-sandbox);

--- src/main/java/com/qa/HFT/factory/OptionsManager.java
+++ src/main/java/com/qa/HFT/factory/OptionsManager.java
@@ -19,7 +19,9 @@ public class OptionsManager {
 		co = new ChromeOptions();
 		//co.setBrowserVersion("116");
 		co.addArguments("--remote-allow-origins=*");
+		co.addArguments("--disable-dev-shm-usage");
 		co.addArguments("--no-sandbox");
+
 		if (Boolean.parseBoolean(prop.getProperty("headless"))) {
 			System.out.println(".....Running the test in Headless mode.......");
 			co.setHeadless(true);
